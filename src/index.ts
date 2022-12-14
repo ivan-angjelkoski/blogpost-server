cbaf00e62e33bc39a02eaff8a7c17cf29634605e
Ivan Angelkoski <56976418+nightexgv@users.noreply.github.com>
2022-12-15 00:55:58 +0100

    Initial Init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app: Application = express();
+const prisma = new PrismaClient();
+
+app.get("/api/blogs", async (req: Request, res: Response) => {
+	const blogs = await prisma.post.findMany();
+	res.json(blogs);
+});
+
+app.get("/api/blog/:id", async (req: Request, res: Response) => {
+	console.log(req.params);
+
+	const id = req.params.id;
+	const blog = await prisma.post.findUnique({
+		where: {
+			id,
+		},
+	});
+	res.json(blog);
+});
+
+app.listen(5000, () => {
+	console.log("Running on port:5000");
+});
